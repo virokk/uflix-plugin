8163df7081d0e93ce6a68bb1e83557315c892370
VRK45 <maxjaan@outlook.com>
2025-07-17 07:49:57 +1200

    Add files via upload

---
+++ UFlix.kt
@@ -0,0 +1,7 @@
+import com.lagradost.cloudstream3.plugins.*
+
+class UFlix : WebViewPlugin() {
+    override val mainUrl = "https://uflix.cc"
+    override val name = "UFlix"
+    override val iconUrl = "https://uflix.cc/favicon.ico"
+}
